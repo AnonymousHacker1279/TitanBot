ea0eb5c5588090521443c6d8a4389c602b179277
Anonymous Hacker <mcgamewarrior@gmail.com>
2021-07-16 14:40:03 -0400

    New commands and bugfixes

--- MiscellaneousCategory.py
+++ MiscellaneousCategory.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import discord
 import json
+import asyncio
 
 class Miscellaneous(commands.Cog):
 	"""Miscellaneous commands that exist for no particular reason."""
@@ -38,7 +39,7 @@ class Miscellaneous(commands.Cog):
 	@commands.command(name='quote')
 	async def quote(self, ctx, ID: int):
 		"""Get a quote by number."""
-		if ID != 0:
+		if ID > 0:
 			with open('quote_list.json', 'r') as f:
 				data = json.load(f)
 				maxIndex = 0
@@ -50,8 +51,7 @@ class Miscellaneous(commands.Cog):
 					response = "You've given me a quote number higher than the number of quotes I have."
 			
 		else:
-			response = "You can't have a quote number of zero."
-		
+			response = "You can't have a quote number lower than one, you brainlet."		
 		await ctx.send(response)
 
 	@commands.command(name='addquote')
@@ -77,25 +77,52 @@ class Miscellaneous(commands.Cog):
 	async def removequote(self, ctx, ID: int):
 		"""Remove a quote from the archive."""
 
-		role = discord.utils.get(ctx.guild.roles, id=865619566334574603)
-		if role in ctx.message.author.roles:
-			with open('quote_list.json', 'r') as f:
-				data = json.load(f)
-				modifiedData = list(data)
-				maxIndex = 0
-				for i in data:
-					maxIndex = maxIndex + 1
-				if ID - 1 < maxIndex:
-					modifiedData.remove(data[ID - 1])
-					flag = True
-				else:
-					response = "You've given me a quote number higher than the number of quotes I have."
+		if ID > 0:
+			role = discord.utils.get(ctx.guild.roles, id=865619566334574603)
+			if role in ctx.message.author.roles:
+				with open('quote_list.json', 'r') as f:
+					data = json.load(f)
+					modifiedData = list(data)
+					maxIndex = 0
 					flag = False
+					for i in data:
+						maxIndex = maxIndex + 1
+					if ID - 1 < maxIndex:
+						modifiedData.remove(data[ID - 1])
+						flag = True
+					else:
+						response = "You've given me a quote number higher than the number of quotes I have."
+						flag = False
 
-			with open('quote_list.json', 'w') as f:
 				if flag == True:
-					json.dump(modifiedData, f, indent=4)
-					response = "Removed quote from archive."
+					with open('quote_list.json', 'w') as f:
+						json.dump(modifiedData, f, indent=4)
+						response = "Removed quote from archive."
+			else:
+				response = "You don't have the proper permissions for this action. Come back when you're more important."
 		else:
-			response = "You don't have the proper permissions for this action. Come back when you're more important."
+			response = "You can't remove a quote number lower than one, you brainlet."
 		await ctx.send(response)
+
+	@commands.command(name='bruh')
+	async def bruh(self, ctx):
+		"""Bruh moment."""
+		response = "https://cdn.discordapp.com/attachments/694603367531544617/865641424665837618/Bruh_Sound_Effect_2.mp3"
+		await ctx.send(response)
+
+	@commands.command(name='oldspice')
+	async def oldspice(self, ctx):
+		"""Smell refreshed."""
+
+		if str(ctx.bot.voice_clients) == "[]":
+			if ctx.author.voice == None:
+				response = "You must be in a voice channel to use this command."
+				await ctx.send(response)
+			else:
+				channel = ctx.author.voice.channel
+				voice = await channel.connect()
+				FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-filter:a "atempo=1" -vn'}
+				voice.play(discord.FFmpegPCMAudio(source='https://cdn.discordapp.com/attachments/694603367531544617/865643189142159420/oldspice.mp3', **FFMPEG_OPTIONS))
+				while voice.is_playing():
+					await asyncio.sleep(1)
+				await ctx.voice_client.disconnect()
