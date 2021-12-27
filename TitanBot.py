cc1eb53fa3a097595e6535c66d90122836cc667f
Anonymous Hacker <mcgamewarrior@gmail.com>
2021-12-27 18:33:58 -0500

    Initial commit
    The bot loads and connects to Discord. The $ping command exists to verify that it's alive, it will use the previous behavior of returning the latency eventually.

---
+++ TitanBot.py
@@ -0,0 +1,18 @@
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("pong")
+
+bot.run(TOKEN)
