c5c72e93d1c581c4a6fb6de1df9d0a9a8d317ecc
Anonymous Hacker <mcgamewarrior@gmail.com>
2021-12-27 18:58:38 -0500

    Move TitanBot.py code to its own class
    Create TitanBot class

--- TitanBot.py
+++ TitanBot.py
@@ -3,16 +3,17 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
+class TitanBot(discord.Client):
+	load_dotenv()
+	TOKEN = os.getenv('DISCORD_TOKEN')
+	GUILD = os.getenv('DISCORD_GUILD')
 
-intents = discord.Intents.default()
-intents.members = True
-bot = commands.Bot(command_prefix='$', intents=intents)
+	intents = discord.Intents.default()
+	intents.members = True
+	bot = commands.Bot(command_prefix='$', intents=intents)
 
-@bot.command()
-async def ping(ctx):
-	await ctx.send("pong")
+	@bot.command()
+	async def ping(ctx):
+		await ctx.send("pong")
 
-bot.run(TOKEN)
+	bot.run(TOKEN)
