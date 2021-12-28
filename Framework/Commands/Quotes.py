5b755b003a2e69bc7c1b9becdb1293da832a20db
Anonymous Hacker <mcgamewarrior@gmail.com>
2021-12-28 15:56:35 -0500

    Add searchQuote functionality
    Add functionality to search quotes by user.

--- Framework/Commands/Quotes.py
+++ Framework/Commands/Quotes.py
@@ -1,10 +1,11 @@
+from discord.errors import NotFound
 from discord.ext import commands
 import discord
 from ..GeneralUtilities.CommandAccess import CommandAccess
 from ..GeneralUtilities import GeneralUtilities as Utilities
 import json
 from random import randint
-
+import math
 
 class Quotes(commands.Cog):
 	
@@ -15,7 +16,7 @@ class Quotes(commands.Cog):
 
 		embed = discord.Embed(color=discord.Color.dark_blue(), description='')
 
-		async def prepareQuote(author, content, id):
+		async def prepare_quote(author, content, id):
 			embed.title = "Quote #" + id
 			
 			if "https://" in content:
@@ -48,7 +49,7 @@ class Quotes(commands.Cog):
 				author = data[random]["author"]
 				content = data[random]["content"]
 
-				embed = await prepareQuote(author, content, str(random))
+				embed = await prepare_quote(author, content, str(random))
 
 			else:
 				try:
@@ -58,7 +59,7 @@ class Quotes(commands.Cog):
 					else:
 						content = data[int(id)]["content"]
 						author = data[int(id)]["author"]
-						embed = await prepareQuote(author, content, id)
+						embed = await prepare_quote(author, content, id)
 				except ValueError:
 					embed.title = "Cannot get quote"
 					embed.description = "The quote ID must be a number."
@@ -67,7 +68,7 @@ class Quotes(commands.Cog):
 
 	@commands.command(name='totalQuotes')
 	@commands.guild_only()
-	async def totalQuotes(self, ctx):
+	async def total_quotes(self, ctx):
 		"""Get the total number of quotes available."""
 
 		embed = discord.Embed(color=discord.Color.dark_blue(), description='')
@@ -90,7 +91,7 @@ class Quotes(commands.Cog):
 
 	@commands.command(name='addQuote')
 	@commands.guild_only()
-	async def addQuote(self, ctx, quote:str, author:str):
+	async def add_quote(self, ctx, quote:str, author:str):
 		"""Did someone say something stupid? Make them remember it with a quote."""
 
 		embed = discord.Embed(color=discord.Color.dark_blue(), description='')
@@ -118,7 +119,7 @@ class Quotes(commands.Cog):
 
 	@commands.command(name='removeQuote')
 	@commands.guild_only()
-	async def removeQuote(self, ctx, id=None):
+	async def remove_quote(self, ctx, id=None):
 		"""Need to purge a quote? Use this. Only available to TitanBot Wizards."""
 
 		embed = discord.Embed(color=discord.Color.dark_blue(), description='')
@@ -156,4 +157,90 @@ class Quotes(commands.Cog):
 				embed.title = "Failed to remove quote"
 				embed.description = "The quote ID must be a number."
 
+		await ctx.send(embed = embed)
+
+	@commands.command(name='searchQuotes')
+	@commands.guild_only()
+	async def search_quotes(self, ctx, quoteAuthor=None, page=1):
+		"""Search quotes by author. Lists up to 5 per page."""
+
+		embed = discord.Embed(color=discord.Color.dark_blue(), description='')
+
+		if await CommandAccess.check_module_enabled("quotes") == False:
+			embed.title = "Cannot use this module"
+			embed.description = "This module has been disabled."
+		else:
+			# Check if an author was provided
+			if quoteAuthor == None:
+				embed.title = "Cannot search quotes"
+				embed.description = "You must provide an author"
+			quoteAuthor = str(quoteAuthor)
+
+			# Get the quote data
+			with open(Utilities.get_quotes_directory(), 'r') as f:
+				data = json.load(f)
+
+				maxIndex = 0
+				# The quote index here lists all quote IDs associated with the author
+				authorQuoteIndex = []
+				for i in data:
+					maxIndex = maxIndex + 1
+					if quoteAuthor in i['author']:
+						authorQuoteIndex.append(maxIndex)
+				maxIndex = maxIndex - 1
+			
+			try:
+				if int(page) < 1:
+					embed.title = "Cannot search quotes"
+					embed.description = "Invalid page. The page must be greater than one."
+				else:
+					authorDisplayName = quoteAuthor
+					try:
+						authorUser = await ctx.bot.fetch_user(int(quoteAuthor.lstrip("<@!").rstrip(">")))
+						authorDisplayName = authorUser.display_name
+						embed.set_thumbnail(url = (authorUser.avatar_url.BASE + authorUser.avatar_url._url))
+					except (NotFound, ValueError):
+						embed.set_footer(text="Cannot get the profile picture for this user, try using a mention")
+
+					embed.title = "Searching Quotes by " + authorDisplayName
+
+					# Check if we're on the first page
+					if page == 1:
+						if len(authorQuoteIndex) != 0:
+							# List the first 5 quotes if the length of the index isn't zero
+							embed.description += "Listing the first five quotes by this author: \n\n"
+							iteration = 0
+							# Iterate through the index and build a response
+							for i in authorQuoteIndex:
+								embed.description += data[authorQuoteIndex[iteration] - 1]["content"] + " **Quote #" + str(authorQuoteIndex[iteration]) + "**\n"
+								iteration = iteration + 1
+								if iteration >= 5:
+									break
+						else:
+							embed.description = "This author doesn't have any quotes."
+					else:
+						# Check if there are enough quotes to fill a page
+						if len(authorQuoteIndex) <= 5 or len(authorQuoteIndex) <= ((page - 1) * 5):
+							embed.description += "This author doesn't have enough quotes to reach this page. \n"
+							embed.description += "They have **" + str(math.ceil(len(authorQuoteIndex) / 5)) + "** pages of quotes."
+						else:
+							# List the next 5 by page number
+							embed.description += "Listing the next five quotes by this author (**Page " + str(page) + "**): \n\n"
+							# Set the iteration by multiplying the page number by 5. First, shift left 1 (as indexes start at 0)
+							iteration = (page - 1) * 5
+							# Iterate through the index and build a response
+							currentQuotesOnPage = 0
+							remainingQuotes = len(authorQuoteIndex) - iteration
+							while remainingQuotes > 0:
+								embed.description += data[authorQuoteIndex[iteration] - 1]["content"] + " **Quote #" + str(authorQuoteIndex[iteration]) + "**\n"
+								iteration = iteration + 1
+								remainingQuotes = remainingQuotes - 1
+								currentQuotesOnPage = currentQuotesOnPage + 1
+								if currentQuotesOnPage >= 5:
+									break
+
+			except ValueError:
+				embed.title = "Cannot search quotes"
+				embed.description = "The page must be a number."
+				
 		await ctx.send(embed = embed)
