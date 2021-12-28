9aad01efe1b31882b44b2ee10d3b41e41c844743
Anonymous Hacker <mcgamewarrior@gmail.com>
2021-12-28 12:51:41 -0500

    More quote system work
    Quotes are now properly zero indexed, meaning quote 0 can exist. Additionally, image handling is now working and the author's PFP will appear in the embed.

--- Framework/Commands/Quotes.py
+++ Framework/Commands/Quotes.py
@@ -12,7 +12,24 @@ class Quotes(commands.Cog):
 	@commands.guild_only()
 	async def quote(self, ctx, id=None):
 		"""Get a random quote, if an ID isn't provided."""
+
 		embed = discord.Embed(color=discord.Color.dark_blue(), description='')
+
+		async def prepareQuote(author, content, id):
+			embed.title = "Quote #" + id
+			
+			if "https://" in content:
+				embed.set_image(url = content)
+				embed.description = author
+			else:
+				embed.description = '> *"' + content + '"*\n'
+				embed.description += " - " + author
+
+			authorUser = await ctx.bot.fetch_user(int(author.lstrip("<@!").rstrip(">")))
+			embed.set_thumbnail(url = (authorUser.avatar_url.BASE + authorUser.avatar_url._url))
+
+			return embed
+
 
 		if await CommandAccess.CommandAccess.check_module_enabled("quotes") == False:
 			embed.title = "Cannot use this module"
@@ -26,24 +43,24 @@ class Quotes(commands.Cog):
 				for _ in data:
 					maxIndex = maxIndex + 1
 			if id == None:
-				random = randint(0, maxIndex - 1)
+				random = randint(0, maxIndex)
 				author = data[random]["author"]
+				content = data[random]["content"]
 
-				embed.title = "Quote #" + str(random)
-				embed.description = '> *"' + data[random]["content"] + '"*\n'
-				embed.description += " - " + author
+				embed = await prepareQuote(author, content, str(random))
+
 			else:
 				try:
-					if int(id) <= 0:
+					if int(id) < 0:
 						embed.title = "Cannot get quote"
-						embed.description = "You must pass an ID above zero."
-					elif int(id) > maxIndex - 1:
+						embed.description = "You must pass an ID that is not less than zero."
+					elif int(id) > maxIndex:
 						embed.title = "Cannot get quote"
 						embed.description = "You are asking for a quote above the number of quotes I have."
 					else:
-						embed.title = "Quote #" + id
-						embed.description = '> *"' + data[int(id) - 1]["content"] + '"*\n'
-						embed.description += " - " + data[int(id) - 1]["author"]
+						content = data[int(id)]["content"]
+						author = data[int(id)]["author"]
+						embed = await prepareQuote(author, content, id)
 				except ValueError:
 					embed.title = "Cannot get quote"
 					embed.description = "You must pass a valid ID."
