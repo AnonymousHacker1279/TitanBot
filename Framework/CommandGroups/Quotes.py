4d926d9d0ab419f7ea3df2746ddd82c606c1e461
Anonymous Hacker <mcgamewarrior@gmail.com>
2022-09-03 19:12:50 -0400

    Implement text search for quotes
    Quotes can now be searched by text content.
    
    Pagination starts at page 0 now, for uniformity with other zero-indexed commands.
    
    Quotes per page increased to 10 results.

--- Framework/CommandGroups/Quotes.py
+++ Framework/CommandGroups/Quotes.py
@@ -48,6 +48,12 @@ class Quotes(commands.Cog):
 					readable_date = "Unknown"
 					pQuotedBy = "Unknown"
 
+				try:
+					int(pQuotedBy)
+					quoted_by_user = str(await ctx.bot.fetch_user(pQuotedBy))
+				except ValueError:
+					quoted_by_user = pQuotedBy
+
 				for _ in links:
 					contentExcludingLinks = re.sub(pattern=links[iteration], repl="", string=pContent)
 					iteration += 1
@@ -55,16 +61,16 @@ class Quotes(commands.Cog):
 					if contentExcludingLinks == "":
 						embed.set_image(url=links[0])
 						embed.description = pAuthor
-						embed.set_footer(text="Added " + readable_date + " by " + str(await ctx.bot.fetch_user(pQuotedBy)))
+						embed.set_footer(text="Added " + readable_date + " by " + quoted_by_user)
 					else:
 						embed.description = '> "' + pContent + '"\n'
 						embed.description += " - <@" + str(pAuthor) + ">"
 						embed.set_image(url=links[0])
-						embed.set_footer(text="Added " + str(readable_date) + " by " + str(await ctx.bot.fetch_user(pQuotedBy)))
+						embed.set_footer(text="Added " + str(readable_date) + " by " + quoted_by_user)
 				else:
 					embed.description = '> "' + pContent + '"\n'
 					embed.description += " - <@" + str(pAuthor) + ">"
-					embed.set_footer(text="Added " + str(readable_date) + " by " + str(await ctx.bot.fetch_user(pQuotedBy)))
+					embed.set_footer(text="Added " + str(readable_date) + " by " + quoted_by_user)
 
 				try:
 					authorUser = await ctx.bot.fetch_user(pAuthor)
@@ -223,20 +229,15 @@ class Quotes(commands.Cog):
 
 		await ctx.respond(embed=embed)
 
-	@commands.slash_command(name='search_quotes')
+	@commands.slash_command(name='search_quotes_author')
 	@commands.guild_only()
-	async def search_quotes(self, ctx: discord.ApplicationContext, quote_author: str = None, page: int = 1):
-		"""Search quotes by author. Lists up to 5 per page."""
+	async def search_quotes_author(self, ctx: discord.ApplicationContext, quote_author: str, page: int = 0):
+		"""Search quotes by author. Lists up to 10 per page."""
 
 		embed = discord.Embed(color=discord.Color.dark_blue(), description='')
 
-		embed, failedPermissionCheck = await PermissionHandler.check_permissions(ctx, embed, "quotes", "search_quotes")
+		embed, failedPermissionCheck = await PermissionHandler.check_permissions(ctx, embed, "quotes", "search_quotes_author")
 		if not failedPermissionCheck:
-			# Check if an author was provided
-			if quote_author is None:
-				embed.title = "Cannot search quotes"
-				embed.description = "You must provide an author"
-
 			quote_author = await GeneralUtilities.strip_usernames(quote_author)
 
 			# Get the quote data
@@ -246,65 +247,124 @@ class Quotes(commands.Cog):
 			# The quote index here lists all quote IDs associated with the author
 			authorQuoteIndex = []
 			for i in data:
-				maxIndex = maxIndex + 1
 				if quote_author in str(i['author']):
 					authorQuoteIndex.append(maxIndex)
+				maxIndex = maxIndex + 1
 
-			try:
-				if int(page) < 1:
-					embed.title = "Cannot search quotes"
-					embed.description = "Invalid page. The page must be greater than one."
-				else:
-					authorDisplayName = quote_author
-					try:
-						authorUser = await ctx.bot.fetch_user(int(quote_author))
-						authorDisplayName = authorUser.display_name
-						embed.set_thumbnail(url=authorUser.display_avatar.url)
-					except (NotFound, ValueError):
-						embed.set_footer(text="Cannot get the profile picture for this user, try using a mention")
-
-					embed.title = "Searching Quotes by " + authorDisplayName
-
-					# Check if we're on the first page
-					if page == 1:
-						if len(authorQuoteIndex) != 0:
-							# List the first 5 quotes if the length of the index isn't zero
-							embed.description += "Listing the first five quotes by this author: \n\n"
-							iteration = 0
-							# Iterate through the index and build a response
-							for _ in authorQuoteIndex:
-								embed.description += data[authorQuoteIndex[iteration] - 1]["content"] + " **Quote #" + str(
-									authorQuoteIndex[iteration] - 1) + "**\n"
-								iteration = iteration + 1
-								if iteration >= 5:
-									break
-						else:
-							embed.description = "This author doesn't have any quotes."
-					else:
-						# Check if there are enough quotes to fill a page
-						if len(authorQuoteIndex) <= 5 or len(authorQuoteIndex) <= ((page - 1) * 5):
-							embed.description += "This author doesn't have enough quotes to reach this page. \n"
-							embed.description += "They have **" + str(
-								math.ceil(len(authorQuoteIndex) / 5)) + "** pages of quotes."
-						else:
-							# List the next 5 by page number
-							embed.description += "Listing the next five quotes by this author (**Page " + str(page) + "**): \n\n"
-							# Set the iteration by multiplying the page number by 5. First, shift left 1 (as indexes start at 0)
-							iteration = (page - 1) * 5
-							# Iterate through the index and build a response
-							currentQuotesOnPage = 0
-							remainingQuotes = len(authorQuoteIndex) - iteration
-							while remainingQuotes > 0:
-								embed.description += data[authorQuoteIndex[iteration] - 1]["content"] + " **Quote #" + str(
-									authorQuoteIndex[iteration] - 1) + "**\n"
-								iteration = iteration + 1
-								remainingQuotes = remainingQuotes - 1
-								currentQuotesOnPage = currentQuotesOnPage + 1
-								if currentQuotesOnPage >= 5:
-									break
-
-			except ValueError:
+			if page < 0:
 				embed.title = "Cannot search quotes"
-				embed.description = "The page must be a number."
+				embed.description = "Invalid page. The page must be greater than zero."
+			else:
+				authorDisplayName = quote_author
+
+				# Try getting a profile picture for the author and a display name
+				try:
+					authorUser = await ctx.bot.fetch_user(int(quote_author))
+					authorDisplayName = authorUser.display_name
+					embed.set_thumbnail(url=authorUser.display_avatar.url)
+				except (NotFound, ValueError):
+					embed.set_footer(text="Cannot get the profile picture for this user, try using a mention")
+
+				embed.title = "Searching Quotes by " + authorDisplayName
+
+				# Check if we're on the first page
+				if page == 0:
+					if len(authorQuoteIndex) != 0:
+						# List the first 10 quotes if the length of the index isn't zero
+						embed.description += "Listing the first ten quotes by this author: \n\n"
+						iteration = 0
+						# Iterate through the index and build a response
+						for _ in authorQuoteIndex:
+							embed.description += data[authorQuoteIndex[iteration]]["content"] + " **Quote #" + str(
+								authorQuoteIndex[iteration]) + "**\n"
+							iteration = iteration + 1
+							if iteration >= 10:
+								break
+					else:
+						embed.description = "This author doesn't have any quotes."
+				else:
+					# Check if there are enough quotes to fill a page
+					if len(authorQuoteIndex) <= 10 or len(authorQuoteIndex) <= (page * 10):
+						embed.description += "This author doesn't have enough quotes to reach this page. \n"
+						embed.description += "They have **" + str(
+							math.ceil(len(authorQuoteIndex) / 10)) + "** pages of quotes."
+					else:
+						# List the next 10 by page number
+						embed.description += "Listing the next ten quotes by this author (**Page " + str(page) + "**): \n\n"
+						# Set the iteration by multiplying the page number by `0. First, shift left 1 (as indexes start at 0)
+						iteration = page * 10
+						# Iterate through the index and build a response
+						currentQuotesOnPage = 0
+						remainingQuotes = len(authorQuoteIndex) - iteration
+						while remainingQuotes > 0:
+							embed.description += data[authorQuoteIndex[iteration]]["content"] + " **Quote #" + str(
+								authorQuoteIndex[iteration]) + "**\n"
+							iteration = iteration + 1
+							remainingQuotes = remainingQuotes
+							currentQuotesOnPage = currentQuotesOnPage + 1
+							if currentQuotesOnPage >= 10:
+								break
+
+		await ctx.respond(embed=embed)
+
+	@commands.slash_command(name='search_quotes_text')
+	@commands.guild_only()
+	async def search_quotes_text(self, ctx: discord.ApplicationContext, text: str, page: int = 0):
+		"""Search quotes by text. Lists up to 10 per page."""
+
+		embed = discord.Embed(color=discord.Color.dark_blue(), description='')
+
+		embed, failedPermissionCheck = await PermissionHandler.check_permissions(ctx, embed, "quotes", "search_quotes_text")
+		if not failedPermissionCheck:
+			# Get the quote data
+			data = await self.cache_managers.get(ctx.guild_id).get_cache()
+
+			maxIndex = 0
+			# The quote index here lists all quote IDs associated with the author
+			quoteIndex = []
+			for i in data:
+				if text.lower() in str(i['content']).lower():
+					quoteIndex.append(maxIndex)
+				maxIndex = maxIndex + 1
+
+			embed.title = "Searching Quotes containing '" + text + "'"
+
+			# Check if we're on the first page
+			if page == 0:
+				if len(quoteIndex) != 0:
+					# List the first 10 quotes if the length of the index isn't zero
+					embed.description += "Listing the first ten quotes found: \n\n"
+					iteration = 0
+					# Iterate through the index and build a response
+					for _ in quoteIndex:
+						embed.description += data[quoteIndex[iteration]]["content"] + " **Quote #" + str(
+								quoteIndex[iteration]) + "**\n"
+						iteration = iteration + 1
+						if iteration >= 10:
+							break
+				else:
+					embed.description = "No quotes were found with the text provided."
+			else:
+				# Check if there are enough quotes to fill a page
+				if len(quoteIndex) <= 10 or len(quoteIndex) <= (page * 10):
+					embed.description += "There aren't enough quotes to reach this page. \n"
+					embed.description += "They have **" + str(
+							math.ceil(len(quoteIndex) / 10)) + "** pages of quotes."
+				else:
+					# List the next 10 by page number
+					embed.description += "Listing the next ten quotes found (**Page " + str(page) + "**): \n\n"
+					# Set the iteration by multiplying the page number by `0. First, shift left 1 (as indexes start at 0)
+					iteration = page * 10
+					# Iterate through the index and build a response
+					currentQuotesOnPage = 0
+					remainingQuotes = len(quoteIndex) - iteration
+					while remainingQuotes > 0:
+						embed.description += data[quoteIndex[iteration]]["content"] + " **Quote #" + str(
+								quoteIndex[iteration]) + "**\n"
+						iteration = iteration + 1
+						remainingQuotes = remainingQuotes
+						currentQuotesOnPage = currentQuotesOnPage + 1
+						if currentQuotesOnPage >= 10:
+							break
 
 		await ctx.respond(embed=embed)
