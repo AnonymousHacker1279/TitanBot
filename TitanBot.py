2860570d731cd1abe7f7183b353ab389419a7094
Anonymous Hacker <mcgamewarrior@gmail.com>
2022-11-10 00:09:09 -0500

    Bump version
    Bump the version to v2.4.0.

--- TitanBot.py
+++ TitanBot.py
@@ -21,7 +21,7 @@ from Framework.Osmium.Osmium import Osmium
 if __name__ == "__main__":
 
 	database_version = 5
-	ConfigurationValues.VERSION = "v2.4.0-indev"
+	ConfigurationValues.VERSION = "v2.4.0"
 	ConfigurationValues.COMMIT = GeneralUtilities.get_git_revision_short_hash()
 
 	intents = discord.Intents.all()
