96777211e48c38bf24e51b7e530829020711bc6c
Nicholas Smit <mcgamewarrior@gmail.com>
2022-12-28 19:58:56 -0500

    Bump version
    Bump the version for a release.

--- TitanBot.py
+++ TitanBot.py
@@ -24,7 +24,7 @@ from Framework.Osmium.Osmium import Osmium
 if __name__ == "__main__":
 
 	database_version = 6
-	ConfigurationValues.VERSION = "v2.5.0-indev"
+	ConfigurationValues.VERSION = "v2.5.0"
 	ConfigurationValues.COMMIT = GeneralUtilities.get_git_revision_short_hash()
 
 	intents = discord.Intents.all()
